0fc94dd1fe611a063c5213b34829e3240aab7681
reggaeguitar <reggaeguitar@users.noreply.github.com>
2015-04-20 17:41:33 -0500

    Initial commit

---
+++ hrGemstones/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace hrGemstones
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numCases = Int32.Parse(Console.ReadLine());
+            var numCasesCopy = numCases;
+            var arr = new int[26];
+            while (numCasesCopy-- > 0)
+            {
+                var line = Console.ReadLine();
+                foreach (var ch in line.Distinct())
+                {
+                    ++arr[ch - 'a'];
+                }
+            }
+            Console.WriteLine(arr.Where(x => x == numCases).Count());
+        }
+    }
+}
